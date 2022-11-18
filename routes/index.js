5e728ae82ba631cac6b3af8d2885e4e8c32a1f9b
SnyDrive25 <sunilgouvi@gmail.com>
2022-11-18 08:55:57 +0100

    final_commit

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,36 @@ var express = require('express');
 var router = express.Router();
 const shell = require('shelljs');
 var fs = require('fs');
+var Airtable = require('airtable');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   var btcusd = shell.exec(`curl https://markets.businessinsider.com/currencies/btc-usd | grep -oP '(?<=<span class="price-section__current-value">).*(?=</span>)'`);
-  var ethusd = shell.exec(`curl https://markets.businessinsider.com/currencies/eth-usd | grep -oP '(?<=<span class="price-section__current-value">).*(?=</span>)'`);
-  var bnbusd = shell.exec(`curl https://markets.businessinsider.com/currencies/bnb-usd | grep -oP '(?<=<span class="price-section__current-value">).*(?=</span>)'`);
+  //var ethusd = shell.exec(`curl https://markets.businessinsider.com/currencies/eth-usd | grep -oP '(?<=<span class="price-section__current-value">).*(?=</span>)'`);
+  //var bnbusd = shell.exec(`curl https://markets.businessinsider.com/currencies/bnb-usd | grep -oP '(?<=<span class="price-section__current-value">).*(?=</span>)'`);
 
   var today = new Date();
   var date = '[' + today.getDay() + '/' + today.getMonth() + '/' + today.getFullYear() + ' | ' + today.getHours() + ':' + today.getMinutes() + ']';
   
   fs.appendFileSync('public/file.txt', date + ' ' + btcusd + '\n');
   
-  res.render('index', { title: 'AutoScrap', currencies: [btcusd, 'btcusd', ethusd, 'ethusd', bnbusd, 'bnbusd'] });
+  var base = new Airtable({apiKey: 'keyZAgEqSGNdXIWLd'}).base('appJ36ZyaFH4MXA96');
+  
+  var output = [];
+
+  base('Table 1').select({
+      maxRecords: 3,
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+          output.push(record.get('value') + "\n");
+      });
+      fetchNextPage();
+  
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
+
+  res.render('index', { title: 'AutoScrap', currencies: [btcusd, 'btcusd'/*, ethusd, 'ethusd', bnbusd, 'bnbusd'*/], base: output });
 });
 
 function curl(url) {
